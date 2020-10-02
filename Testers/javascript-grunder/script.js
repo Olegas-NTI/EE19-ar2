19b9e5fa138a0b59b1e00209c0cd9c6b7c69f469
Olegas Sevcenko <olegas.sevcenko@learnet.se>
2020-10-02 12:36:33 +0200

    Sortering för klara och oklara testers

--- Testers/javascript-grunder/script.js
+++
@@ -1,9 +0,0 @@
-const firstNumber = 10
-let secondNumber = 5
-
-answer = firstNumber + secondNumber
-
-/* firstNumber = 5  <---- Ger error pga const variabel får inte bli ändrat. */
-
-/* secondNumber = 10 <---- Får att bli ändrat pga våran variabel typ är "let" */
-console.log(answer);
