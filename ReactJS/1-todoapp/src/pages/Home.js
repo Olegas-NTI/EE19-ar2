3f596fe47c0e153d85a7f89b4d0c4d12436a0557
Olegas-NTI <olegas.sevcenko@elev.ga.ntig.se>
2020-12-28 19:15:09 +0100

    Fixa grejer

--- ReactJS/1-todoapp/src/pages/Home.js
+++ ReactJS/1-todoapp/src/pages/Home.js
@@ -17,6 +17,7 @@ export const Home = () => {
             <Form />
 
             <hr/>
+            <ul className="list-group"><li className="list-group-item note"><div><strong>Anteckningar är anslutna till Firebase database'n så att dom inte försvinner :)</strong><small>2020-12-28T02:13:34.856Z</small></div><button type="button" className="btn btn-outline-danger" disabled>×</button></li></ul>
 
             {loading 
               ? <Loader />
