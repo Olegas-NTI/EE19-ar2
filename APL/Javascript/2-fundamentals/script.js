0d85a6eec8937b3faa36103a737d19c9c3a19d5a
Olegas-NTI <olegas.sevcenko@elev.ga.ntig.se>
2021-02-25 16:31:52 +0100

    har gått igenom funktioner!

---
+++ APL/Javascript/2-fundamentals/script.js
@@ -0,0 +1,50 @@
+'use strict';
+/* 
+// make function
+function logger() {
+    console.log("My name is Olegas");
+}
+// calling / running / invoking function
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`
+    return juice // Try without
+}
+
+const appleJuice = fruitProcessor(10, 1)
+console.log(appleJuice);
+const orangeJuice = fruitProcessor(1, 15)
+console.log(orangeJuice);
+const textToNumber = Number('25') + 25
+console.log(textToNumber); 
+*/
+
+// Function declaration 
+/* 
+const age1 = calcAge1(2003);
+function calcAge1(birthYear) {
+    return 2021 - birthYear;
+}
+
+// Function expression
+const calcAge2 = function (birthYear) {
+    return 2021 - birthYear
+}
+const age2 = calcAge2(2003)
+console.log(age1, age2);
+
+// Arrow functions
+const calcAge3 = birthYear => 2037 - birthYear
+const age3 = calcAge3(2003);
+console.log(age3);
+
+const yearsUntilRetirement = birthYear => {
+    const age = 2021 - birthYear;
+    const retirement = 65 - age;
+    return retirement;
+}
+
+console.log(yearsUntilRetirement(2003)); 
+*/
