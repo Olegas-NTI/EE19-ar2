f5875bff69528b26ec162ccff0602c7c18bbcc29
Olegas Sevcenko <olegas.sevcenko@elev.ga.ntig.se>
2021-04-30 16:20:16 +0200

    graphql och styling

--- APL/Gatsby/gatsby-cms/src/pages/index.js
+++ APL/Gatsby/gatsby-cms/src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { graphql, useStaticQuery, Link } from "gatsby"
+import * as mainStyles from "./index.module.css"
+import "./index.css"
 
 const pageQuery = graphql`
   {
@@ -8,6 +10,8 @@ const pageQuery = graphql`
         name
         slug
         price
+        category
+        stock
         images {
         thumbnail: url(
         transformation: {
@@ -22,26 +26,46 @@ const pageQuery = graphql`
   }
 `
 
+
+
 const IndexPage = () => {
   const {
     gcms: { products },
   } = useStaticQuery(pageQuery)
-  console.log(products)
-  
   return (
     <div>
-      {products.map(({ slug, ...product }) => (
-        <div>
-          <Link key={slug} to={`/products/${slug}`}>
-          {product.name}
-        </Link>
-        <div><img src={product.images == null ? `https://layali.se/wp-content/plugins/lightbox/images/No-image-found.jpg` : product.images.thumbnail} alt={product.name}/></div>
-        </div>
-      
-        
-      ))}
+      <h1 className={mainStyles.productsHeader}>Products</h1>
+      <div className={mainStyles.container}>
+        {products.map(({ slug, ...product }) => (
+          <div className={mainStyles.cardStyle}>
+            <div className={mainStyles.cardImagebox}>
+            <img
+              src={
+                product.images == null
+                  ? `https://dummyimage.com/250/ffffff/000000`
+                  : product.images.thumbnail
+              }
+              alt={product.name}
+            />  
+            </div>
+            <div className={mainStyles.productNameBlock}>
+              <Link
+                class={mainStyles.productName}
+                key={slug}
+                to={`/products/${slug}`}
+              >
+                {product.name}
+              </Link>
+              <div className={mainStyles.greyline}></div>
+              <div>
+                {<span className={product.category}>{product.category}</span>}
+                <span className={product.stock ? mainStyles.inStock : mainStyles.outOfStock}>{product.stock ? 'In stock' : 'Out of stock'}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  
   )
 }
 
--- APL/Gatsby/gatsby-cms/src/templates/ProductPage.js
+++ APL/Gatsby/gatsby-cms/src/templates/ProductPage.js
@@ -1,28 +1,48 @@
 import React from "react"
-import { graphql } from "gatsby"
-
+import { graphql, Link} from "gatsby"
+import * as mainStyles from "./ProductPage.module.css"
+import './productpage.css'
+import '../pages/index.css'
 const ProductsPage = ({
   data: {
     gcms: { product },
   },
 }) => (
   <React.Fragment>
-    <h1>{product.name}</h1>
-    <p>{product.description}</p>
-    <h1>{product.price}</h1>
+    <div className={mainStyles.imageContainer}>
+      <img src={product.images == null ? 'https://i.stack.imgur.com/6M513.png' : product.images.url} alt={product.name}/>
+    </div>
+
+    <div className={mainStyles.container}>
+      <div className={`${mainStyles.linkBackBlock} ${product.category}`} >
+      <Link className={mainStyles.linkBack} to={'/'}>Go back</Link>
+      </div>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <h1>{product.price} kr</h1>
+    </div>
   </React.Fragment>
 )
 
-
 export const pageQuery = graphql`
-    query ProductPageQuery($id: ID!) {
-        gcms {
-            product(where: {id: $id}) {
-                name
-                description
-                price
+  query ProductPageQuery($id: ID!) {
+    gcms {
+      product(where: { id: $id }) {
+        name
+        description
+        price
+        category
+        images {
+          thumbnail: url(
+            transformation: {
+              image: { resize: { width: 1250, height: 750, fit: clip } }
+              document: { output: { format: jpg } }
             }
+          )
+          url(transformation: { document: { output: { format: jpg } } })
         }
+      }
     }
-`;
+  }
+`
 export default ProductsPage
