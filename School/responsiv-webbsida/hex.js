aa6ede468d2aa8d19cc3d252323812f4be827fbd
olegas-sev <olegas.sevcenko@elev.ga.ntig.se>
2021-05-25 00:57:52 +0200

    Jobbade på projekt

---
+++ School/responsiv-webbsida/hex.js
@@ -0,0 +1,12 @@
+const hexInput = document.querySelector(".hex-input");
+const hexCanvas = document.querySelector(".hex-canvas");
+const errorMessage = document.querySelector(".error-message")
+hexInput.addEventListener("input", function () {
+    if (hexInput.value.includes("#")) {
+        console.error("Ta bort hashtag'en!");
+        errorMessage.textContent = "Ta bort hashtag'en!"
+    } else if (hexInput.value.length >= 3 && hexInput.value.includes("#") == false) {
+        errorMessage.textContent = ""
+        hexCanvas.style.backgroundColor =  "#" + hexInput.value
+    }
+})
