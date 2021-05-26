7faa71901e8136b40e306732562ad465e981970f
Olegas Sevcenko <olegas.sevcenko@elev.ga.ntig.se>
2021-05-26 11:36:13 +0200

    Arbetade med projekt

--- School/responsiv-webbsida/calculator.js
+++ School/responsiv-webbsida/calculator.js
@@ -1,7 +1,7 @@
 
-const calcOutput = document.querySelector(".calc-output")
-const buttons = document.querySelectorAll(".calc-btn")
-const enter = document.querySelector(".enter")
+const calcOutput = document.querySelector(".calc-output");
+const buttons = document.querySelectorAll(".calc-btn");
+const enter = document.querySelector(".enter");
 let numberBoxEtt = "";
 let numberBoxTvå = "";
 let operation;
@@ -9,44 +9,57 @@ let nummerTvå;
 let nummerEtt;
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
+        calcOutput.value = ""
         if (!isNaN(buttons[i].value) && !operation) {
-            numberBoxEtt = numberBoxEtt + buttons[i].value
+            numberBoxEtt = numberBoxEtt + buttons[i].value;
             nummerEtt = Number(numberBoxEtt);
         } else if (isNaN(buttons[i].value)) {
-            operation = buttons[i].value
-            for (let i = 0; i < buttons.length; i++) {
-                buttons[i].addEventListener("click", function () {
-                    if (!isNaN(buttons[i].value)) {
-                        numberBoxTvå = numberBoxTvå + buttons[i].value
-                        nummerTvå = Number(numberBoxTvå)
-                    }
-                })
-            }
-        } 
-    })          
+            operation = buttons[i].value;
+
+        } else if (!isNaN(buttons[i].value)) {
+            numberBoxTvå = numberBoxTvå + buttons[i].value;
+            nummerTvå = Number(numberBoxTvå);
+        }
+        calcOutput.value = buttons[i].value;
+    });
 }
 
 
 enter.addEventListener("click", function () {
     if (operation == "+") {
-        calcOutput.value = nummerEtt + nummerTvå
+        calcOutput.value = nummerEtt + nummerTvå;
         const li = document.createElement('li');
-        li.innerText = `${nummerEtt} + ${nummerTvå} = ${calcOutput.value}`
+        li.innerText = `${nummerEtt} + ${nummerTvå} = ${calcOutput.value}`;
         document.querySelector(".history-list").appendChild(li);
-    } 
+        reset()
+    }
     if (operation == "-") {
-        calcOutput.value = nummerEtt - nummerTvå
+        calcOutput.value = nummerEtt - nummerTvå;
+        const li = document.createElement('li');
+        li.innerText = `${nummerEtt} - ${nummerTvå} = ${calcOutput.value}`;
+        document.querySelector(".history-list").appendChild(li);
+        reset()
     }
     if (operation == "/") {
-        calcOutput.value = nummerEtt / nummerTvå
+        calcOutput.value = nummerEtt / nummerTvå;
+        const li = document.createElement('li');
+        li.innerText = `${nummerEtt} / ${nummerTvå} = ${calcOutput.value}`;
+        document.querySelector(".history-list").appendChild(li);
+        reset()
     }
     if (operation == "*") {
-        calcOutput.value = nummerEtt * nummerTvå
+        calcOutput.value = nummerEtt * nummerTvå;
+        const li = document.createElement('li');
+        li.innerText = `${nummerEtt} * ${nummerTvå} = ${calcOutput.value}`;
+        document.querySelector(".history-list").appendChild(li);
+        reset()
     }
+});
+
+const reset = function() {
     numberBoxEtt = "";
     numberBoxTvå = "";
-    operation = undefined;
+    operation = "";
     nummerTvå = "";
-    nummerEtt = "";
-})
-
+    nummerEtt = "";    
+} 
