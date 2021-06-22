cb87d1c58b99b120e9d0ca5505906055e70714a7
Olegas Sevcenko <olegas.sevcenko@elev.ga.ntig.se>
2021-06-22 19:18:37 +0200

    Worked with closures, bind, call & apply methods

--- APL/Javascript/10-functions/script.js
+++ APL/Javascript/10-functions/script.js
@@ -83,3 +83,116 @@ const subtraction = function (value1, value2) {
 
 calculate(2, 5, addition)
 calculate(2, 5, subtraction) */
+
+
+/*
+// Closures
+ const greet = function name(greeting) {
+    return function(name) {
+        console.log(`${greeting} ${name}!`);
+    }
+}
+
+const greeterHey = greet('Hey!')
+greeterHey('Olegas')
+greet('Hello')('Maram')
+
+
+// Challenge
+const greetArrow = (greeting) =>{
+    return (name) => {
+        console.log(`${greeting} ${name}`);
+    }
+}
+// Teacher's method
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArrow('Hiiii!')('Olegas')
+greetArr('Hey!')('Olegas')
+*/
+
+// Call Apply and Bind Methods 
+
+const lufthansa = {
+    airline: 'lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    book: function (flightNum, name) {
+        console.log(`${name} booked a seat on ${this.lufthansa} flight ${this.iataCode}${flightNum}`);
+        this.bookings.push({flight: `${this.iataCode}${flightNum}`, name})
+    }
+}
+
+lufthansa.book(239, 'Jonas Schmedtmann')
+lufthansa.book(635, 'John Smith')
+const eurowings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings: [],
+    
+}
+const book = lufthansa.book;
+
+// Does not work
+// book(23, 'Maram Al Sth')
+
+// Call Method
+book.call(eurowings, 223, 'Maram Al Sth')
+book.call(lufthansa, 239, 'Olegas Sevcenko')
+
+const swiss = {
+    airline: `Swiss Air Lines`,
+    iataCode: `LX`,
+    bookings: []
+}
+
+book.call(swiss, 530, 'Tommy Shelby')
+
+// Apply Method (not used in modern js)
+const flightData = [583, 'George Georgerian']
+book.apply(swiss, flightData)
+// We use call method instead, if we want to use array as in apply method, we can use spread operator ... in order to pass in data
+book.call(swiss, ...flightData)
+
+// Bind method
+
+const bookEW = book.bind(eurowings)
+const bookLH = book.bind(lufthansa)
+const bookLX = book.bind(swiss)
+// Partial application available in bind method
+const bookEW23 = book.bind(eurowings, 23)
+bookEW(23, 'Steven Imaginary')
+bookEW23('Olegas Kapchenko')
+
+console.log(lufthansa);
+console.log(eurowings);
+console.log(swiss);
+
+// With Event Listeners 
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+    console.log(this);
+    this.planes++
+    console.log(this.planes);
+}
+
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa))
+
+// Partial appplication
+const addTax = (rate, value) => value + value * rate
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23)
+// addVAT = value => value + value * 0.23
+
+console.log(addVAT(100));
+// Challenge, make a closure that would do same as bind function
+const addNEWTAX = function (rate) {
+    return function (value) {
+        console.log(value + value * rate);
+    }
+}
+
+addNEWTAX(0.23)(100)
+const addNEWVAT = addNEWTAX(0.23)
+addNEWVAT(100)
