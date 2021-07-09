279506c14890d1924ab4059a6c6e52b2f79759be
olegas-sev <olegas.sevcenko@elev.ga.ntig.se>
2021-07-09 14:27:05 +0200

    Worked with classes

--- APL/Javascript/13-advanced-dom/script.js
+++ APL/Javascript/13-advanced-dom/script.js
@@ -70,3 +70,46 @@ document.querySelector('.btn--close-cookie').addEventListener('click', () => {
   // Old way to remove elements
   // message.parentElement.removeChild(message)
 })
+
+// Styles 
+message.style.backgroundColor = '#37383d'
+message.style.width = '100%'
+console.log(message.style.width);
+console.log(message.style.height);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+message.style.height = parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered')
+
+
+// Attributes
+const logo = document.querySelector('.nav__logo')
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+// Non standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+console.log(logo.setAttribute('company', 'Bankist'));
+
+console.log(logo.src); // => https://localhost:8080/img/logo.png
+console.log(logo.getAttribute('src')); // => img/logo.png 
+
+const link = document.querySelector('.nav__link--btn')
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'd')
+logo.classList.remove('c', 'd')
+logo.classList.toggle('c')
+logo.classList.contains('c') // not includes like in arrrays
+
+// Dont use it because it overwrites the whole class property
+logo.className = 'olegas'
